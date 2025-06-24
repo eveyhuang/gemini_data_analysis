3c6a42c5a42218513e8c70d8285f59371706ca2b
Evey Huang <jiaxinh.evey@gmail.com>
2025-06-24 15:13:15 -0500

    job

--- process_video_job.sh
+++ process_video_job.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
-#SBATCH -A <allocationID>
-#SBATCH --partition=<queueName>
-#SBATCH --time=<hh:mm:ss>
-#SBATCH --mail-user=<emailAddress>
-#SBATCH --output=<combined out and err file path>
-#SBATCH -J <jobName>
+#SBATCH -A p32824
+#SBATCH --partition=gengpu
+#SBATCH --time=02:00:00
+#SBATCH --mail-user=evey.huang@kellogg.northwestern.edu
 #SBATCH --nodes=1
-#SBATCH -n <core count>
-#SBATCH --mem=<memory in MB>
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:a100:1
+#SBATCH --mem=2G
+#SBATCH --cpus-per-task=2
+#SBATCH --job-name=process_video
 
 # unload modules that may have been loaded when job was submitted
 module purge all
+module load mamba
+source activate gemini
 
-# load the version of python you want to use
-module load python/anaconda3.6
+source .env
+echo "GOOGLE_API_KEY: $GOOGLE_API_KEY"
 
 # By default all file paths are relative to the directory where you submitted the job.
 # To change to another path, use `cd <path>`, for example:
 # cd /projects/<allocationID>
 
-python myscript_named.py --intval=${ARG1} --stringval=${ARG2}
+python analyze_video.py --dir "/home/jhs0727/p32824/scialog/2021MND" --process-video yes --annotate-video no
 
-# OR
-# python myscript_named.py -i ${ARG1} -s ${ARG2}
